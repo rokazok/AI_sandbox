88c3c5c360d5670fdc5fc1a537e995ff261ed643
Remy Okazaki <>
2025-03-28 08:46:45 -0700

    update to python3 commands

--- setup/setup_virtual_env.sh
+++ setup/setup_virtual_env.sh
@@ -17,7 +17,7 @@ pyenv virtualenv 3.12.7 sandbox
 # Create a .python-version file in the root of the project to autoload the virtual environment
 pyenv local sandbox
 
-pip install -r setup/requirements.txt
+pip3 install -r setup/requirements.txt
 
 # Set up the kernel for Jupyter
-python -m ipykernel install --user --name=sandbox
+python3 -m ipykernel install --user --name=sandbox
